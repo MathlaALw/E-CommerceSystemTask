faae977fe4273414d6d4463ed08dc8c0a8cc9302
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-09-03 01:33:20 +0400

    Add MappingProfile for DTO and model mappings

---
+++ MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+
+namespace E_CommerceSystem
+{
+    public class MappingProfile : Profile 
+    {
+
+        public MappingProfile()
+        {
+            CreateMap<Models.UserDTO, Models.User>();
+            CreateMap<Models.User, Models.UserDTO>();
+
+            CreateMap<Models.CategoryDTO, Models.Category>();
+            CreateMap<Models.Category, Models.CategoryDTO>();
+
+            CreateMap<Models.ProductDTO, Models.Product>();
+            CreateMap<Models.Product, Models.ProductDTO>();
+
+
+
+        }
+    }
+}
