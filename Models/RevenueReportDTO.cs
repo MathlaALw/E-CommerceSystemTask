e5f09a24116eecf0cdaafef8eae2e90fca7acd5d
Amani Khalid <amanikhalidd@gmail.com>
2025-09-03 10:18:52 +0400

    Added PeriodType

--- Models/RevenueReportDTO.cs
+++ Models/RevenueReportDTO.cs
@@ -3,5 +3,6 @@
     public class RevenueReportDTO
     {
         public DateTime Period { get; set; } // Period (e.g., Month, Year) 
+        public string PeriodType { get; set; } // Period Type (e.g., "Monthly", "Yearly")
     }
 }
