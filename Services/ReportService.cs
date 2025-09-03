2deccdc62c52bbf41a2637cebcf7cc3ac957095c
Amani Khalid <amanikhalidd@gmail.com>
2025-09-03 10:58:01 +0400

    Query to get orders

--- Services/ReportService.cs
+++ Services/ReportService.cs
@@ -39,6 +39,10 @@ namespace E_CommerceSystem.Services
 
         public IEnumerable<RevenueReportDTO> GetRevenueReport(DateTime startDate, DateTime endDate, string periodType = "daily") // Get revenue report within a date range
         {
+            var orders = _context.Orders // Query to get orders
+               .Where(o => o.OrderDate >= startDate && o.OrderDate <= endDate && o.Status != OrderStatus.Cancelled)
+               .ToList();
+
 
 
 
