78a4a47a02c39dce0bccc1e35068de53134e7080
Amani Khalid <amanikhalidd@gmail.com>
2025-09-03 15:45:03 +0400

    Order of display if multiple images exist

--- Models/ProductImageDTO.cs
+++ Models/ProductImageDTO.cs
@@ -5,5 +5,7 @@
         public int ImageId { get; set; } // Unique identifier for each image
         public string ImageUrl { get; set; } // Image URL or path
         public bool IsMain { get; set; } // Indicates if this is the main image for the product
+
+        public int DisplayOrder { get; set; } // Order of display if multiple images exist
     }
 }
