3a9a9a13839e1bf2bdfcf346eaa87ac711b6a78c
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-09-03 11:42:09 +0400

    Revokes a single refresh token, optionally noting what token replaced it.  This method is useful for invalidating a specific token

--- Repositories/IRefreshTokenRepo.cs
+++ Repositories/IRefreshTokenRepo.cs
@@ -8,7 +8,8 @@ namespace E_CommerceSystem.Repositories
         RefreshToken GetRefreshToken(string token);   // Retrieves a refresh token from the repository using its string value
         void RevokeDescendantRefreshTokens(RefreshToken refreshToken, string ipAddress, string reason); //Revokes all descendant refresh tokens associated with a given token.
                                                                                                         // Descendant tokens are those created from a previous token in the same chain.
-        void RevokeRefreshToken(RefreshToken token, string ipAddress, string reason = null, string replacedByToken = null);
+        void RevokeRefreshToken(RefreshToken token, string ipAddress, string reason = null, string replacedByToken = null); // Revokes a single refresh token, optionally noting what token replaced it.
+                                                                                                                            // This method is useful for invalidating a specific token
         void UpdateRefreshToken(RefreshToken refreshToken);
     }
 }
