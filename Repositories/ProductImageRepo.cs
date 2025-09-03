d65780fd5e2c408701631b89ec212cb1cc988495
Amani Khalid <amanikhalidd@gmail.com>
2025-09-03 15:52:22 +0400

    Constructor with dependency injection

--- Repositories/ProductImageRepo.cs
+++ Repositories/ProductImageRepo.cs
@@ -3,6 +3,11 @@
     public class ProductImageRepo
     {
         private readonly ApplicationDbContext _context; // Database context
+        public ProductImageRepo(ApplicationDbContext context) // Constructor with dependency injection
+        {
+            _context = context;
+        }
+
 
     }
 }
