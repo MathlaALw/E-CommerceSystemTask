8b2ac1aac1d09cd04654acecd2532442e316a592
Amani Khalid <amanikhalidd@gmail.com>
2025-09-03 10:27:10 +0400

    Number of Orders

--- Models/ActiveCustomerDTO.cs
+++ Models/ActiveCustomerDTO.cs
@@ -5,6 +5,7 @@
         public int UserId { get; set; } // User ID
         public string UserName { get; set; } // User Name
         public string Email { get; set; } // User Email
+        public int OrderCount { get; set; } // Number of Orders
 
     }
 }
