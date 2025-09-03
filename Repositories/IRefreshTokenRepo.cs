dc747e1a6b534280912950d7ffec47a7ab3248a9
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-09-03 11:41:36 +0400

    Revokes all descendant refresh tokens associated with a given token.  Descendant tokens are those created from a previous token in the same chain.

--- Repositories/IRefreshTokenRepo.cs
+++ Repositories/IRefreshTokenRepo.cs
@@ -6,7 +6,8 @@ namespace E_CommerceSystem.Repositories
     {
         void AddRefreshToken(RefreshToken refreshToken); // Adds a new refresh token to the repository.
         RefreshToken GetRefreshToken(string token);   // Retrieves a refresh token from the repository using its string value
-        void RevokeDescendantRefreshTokens(RefreshToken refreshToken, string ipAddress, string reason);
+        void RevokeDescendantRefreshTokens(RefreshToken refreshToken, string ipAddress, string reason); //Revokes all descendant refresh tokens associated with a given token.
+                                                                                                        // Descendant tokens are those created from a previous token in the same chain.
         void RevokeRefreshToken(RefreshToken token, string ipAddress, string reason = null, string replacedByToken = null);
         void UpdateRefreshToken(RefreshToken refreshToken);
     }
