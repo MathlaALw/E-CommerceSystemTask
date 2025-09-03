d474b7339929335dcf1149f67469b985c9cb2b29
Amani Khalid <amanikhalidd@gmail.com>
2025-09-03 10:20:44 +0400

    Average Order Value

--- Models/RevenueReportDTO.cs
+++ Models/RevenueReportDTO.cs
@@ -6,5 +6,6 @@
         public string PeriodType { get; set; } // Period Type (e.g., "Monthly", "Yearly")
         public int TotalOrders { get; set; } // Total Number of Orders
         public decimal TotalRevenue { get; set; } // Total Revenue Generated
+        public decimal AverageOrderValue { get; set; } // Average Order Value
     }
 }
