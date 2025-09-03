b3bc3f4004096f70926fb821913bd40fa98c09af
Amani Khalid <amanikhalidd@gmail.com>
2025-09-03 11:41:46 +0400

    Created Main Structure

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -1,4 +1,11 @@
-﻿namespace E_CommerceSystem.Controllers
+﻿using E_CommerceSystem.Models;
+using E_CommerceSystem.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace E_CommerceSystem.Controllers
 {
     public class ReportController
     {
