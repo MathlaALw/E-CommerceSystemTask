e8db357e952334065b0901aa5994d5fd4076c7fd
Amani Khalid <amanikhalidd@gmail.com>
2025-09-03 15:43:52 +0400

    Image URL or path

--- Models/ProductImageDTO.cs
+++ Models/ProductImageDTO.cs
@@ -3,6 +3,6 @@
     public class ProductImageDTO
     {
         public int ImageId { get; set; } // Unique identifier for each image
-        
+        public string ImageUrl { get; set; } // Image URL or path
     }
 }
