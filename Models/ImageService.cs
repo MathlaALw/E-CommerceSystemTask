a5c669fd548936664152cd6a1a683949042f0f06
Amani Khalid <amanikhalidd@gmail.com>
2025-09-03 16:26:25 +0400

    Constructor with dependency injection

--- Models/ImageService.cs
+++ Models/ImageService.cs
@@ -2,5 +2,14 @@
 {
     public class ImageService
     {
+        private readonly IWebHostEnvironment _environment; // To access web hosting environment details
+        private readonly IConfiguration _configuration; // To access configuration settings
+
+        public ImageService(IWebHostEnvironment environment, IConfiguration configuration) // Constructor with dependency injection
+        {
+            _environment = environment;
+            _configuration = configuration;
+        }
+
     }
 }
