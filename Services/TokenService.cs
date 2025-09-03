1fe00a84655c4ded6064c0ddd4d901c8214f6ce2
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-09-03 13:06:24 +0400

    Declares a method to generate a new JWT (JSON Web Token) for a given user

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,6 +1,19 @@
-﻿namespace E_CommerceSystem.Services
+﻿using E_CommerceSystem.Models;
+using E_CommerceSystem.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace E_CommerceSystem.Services
 {
     public class TokenService
     {
+        // Declares a method to generate a new JWT (JSON Web Token) for a given user.
+        string GenerateJwtToken(User user);
     }
 }
