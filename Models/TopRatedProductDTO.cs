d184dfd88244595dde53af13d291b113892bc2d0
Amani Khalid <amanikhalidd@gmail.com>
2025-09-03 10:23:49 +0400

    Average Rating

--- Models/TopRatedProductDTO.cs
+++ Models/TopRatedProductDTO.cs
@@ -4,6 +4,7 @@
     {
         public int ProductId { get; set; } // Product ID
         public string ProductName { get; set; } // Product Name
+        public decimal AverageRating { get; set; } // Average Rating
 
     }
 }
