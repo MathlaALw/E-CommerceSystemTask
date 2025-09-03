fec06ebef3ac64bce71a08ca946ecd67d129f8d9
Amani Khalid <amanikhalidd@gmail.com>
2025-09-03 12:07:26 +0400

    Get top 5 best-selling products this month

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -120,7 +120,7 @@ namespace E_CommerceSystem.Controllers
                     .Count(o => o.OrderDate >= monthStart && o.OrderDate <= monthEnd && o.Status != OrderStatus.Cancelled); // Count orders in the month excluding cancelled
 
                 // Best selling products this month
-                var bestSellers = _reportService.GetBestSellingProducts(monthStart, monthEnd, 5);
+                var bestSellers = _reportService.GetBestSellingProducts(monthStart, monthEnd, 5); // Get top 5 best-selling products this month
 
 
 
