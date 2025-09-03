b9a81c375f776393995ecfca69a449e71080e303
Amani Khalid <amanikhalidd@gmail.com>
2025-09-03 15:44:38 +0400

    Indicates if this is the main image for the product

--- Models/ProductImageDTO.cs
+++ Models/ProductImageDTO.cs
@@ -4,5 +4,6 @@
     {
         public int ImageId { get; set; } // Unique identifier for each image
         public string ImageUrl { get; set; } // Image URL or path
+        public bool IsMain { get; set; } // Indicates if this is the main image for the product
     }
 }
