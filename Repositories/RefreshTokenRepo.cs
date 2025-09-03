b162fecd5d35e267f686e398e4a707ae7a03d1ea
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-09-03 11:30:02 +0400

    Revokes all descendant tokens in a token chain (rotation scenario)

--- Repositories/RefreshTokenRepo.cs
+++ Repositories/RefreshTokenRepo.cs
@@ -46,6 +46,9 @@ namespace E_CommerceSystem.Repositories
             _context.RefreshTokens.Update(token);         // Update token entity
             _context.SaveChanges();                       // Save changes
 
+        // Revokes all descendant tokens in a token chain (rotation scenario)
+         public void RevokeDescendantRefreshTokens(RefreshToken refreshToken, string ipAddress, string reason)
+        {
 
 
 
