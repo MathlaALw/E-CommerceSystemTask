36d8274aa48ea09dc822588dbc9872d1bbe5585b
Amani Khalid <amanikhalidd@gmail.com>
2025-09-03 10:32:21 +0400

    Created main structure

--- Services/ReportService.cs
+++ Services/ReportService.cs
@@ -1,4 +1,12 @@
-﻿namespace E_CommerceSystem.Services
+﻿using E_CommerceSystem.Models;
+using E_CommerceSystem.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace E_CommerceSystem.Services
 {
     public class ReportService
     {
