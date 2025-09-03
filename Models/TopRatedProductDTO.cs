3f53ebcd28944f7fd69b552de8a4659f629d61b8
Amani Khalid <amanikhalidd@gmail.com>
2025-09-03 10:24:19 +0400

    Number of Reviews

--- Models/TopRatedProductDTO.cs
+++ Models/TopRatedProductDTO.cs
@@ -5,6 +5,7 @@
         public int ProductId { get; set; } // Product ID
         public string ProductName { get; set; } // Product Name
         public decimal AverageRating { get; set; } // Average Rating
+        public int ReviewCount { get; set; } // Number of Reviews
 
     }
 }
