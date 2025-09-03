ca0e2baa342b53dcc99ed7dabf176c58c58986e8
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-09-03 15:11:01 +0400

    Extract token from cookie or header

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using E_CommerceSystem.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
@@ -66,9 +67,14 @@ namespace E_CommerceSystem
                             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)) // Match with your token generation key.
                         };
                     });
+            // Extract token from cookie or header
+            options.Events = new JwtBearerEvents
+            {
+                OnMessageReceived = context =>
+                {
 
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
+                    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                    builder.Services.AddEndpointsApiExplorer();
 
             builder.Services.AddSwaggerGen(c =>
             {
