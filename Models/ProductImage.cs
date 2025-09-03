66b2a26f194b6582ef0c6730926a99c4b716d6d9
Amani Khalid <amanikhalidd@gmail.com>
2025-09-03 15:31:15 +0400

    Added Primary Key

--- Models/ProductImage.cs
+++ Models/ProductImage.cs
@@ -1,7 +1,26 @@
-﻿namespace E_CommerceSystem.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace E_CommerceSystem.Models
 {
     public class ProductImage
     {
+        [Key] // Primary Key
+        public int ImageId { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        public bool IsMain { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        [ForeignKey("Product")]
+        public int PID { get; set; }
+
+        [JsonIgnore]
+        public Product Product { get; set; }
 
     }
 }
