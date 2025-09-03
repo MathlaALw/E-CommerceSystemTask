eda41e801db4f36282d63fc7b7ec9b65e8adc709
Amani Khalid <amanikhalidd@gmail.com>
2025-09-03 16:30:05 +0400

    Method to save an image file asynchronously

--- Models/ImageService.cs
+++ Models/ImageService.cs
@@ -11,5 +11,13 @@
             _configuration = configuration; // Access to configuration settings
         }
 
+        public async Task<string> SaveImageAsync(IFormFile imageFile, string subFolder = "") // Method to save an image file asynchronously
+        {
+            if (imageFile == null || imageFile.Length == 0)
+                throw new ArgumentException("No image file provided");
+
+            if (!IsValidImage(imageFile))
+                throw new ArgumentException("Invalid image file");
+
+        }
     }
-}
