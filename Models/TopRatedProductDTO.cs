60c71fb4664b00c7540a534ac8918d9c6f290b98
Amani Khalid <amanikhalidd@gmail.com>
2025-09-03 10:23:26 +0400

    Product Name

--- Models/TopRatedProductDTO.cs
+++ Models/TopRatedProductDTO.cs
@@ -3,6 +3,7 @@
     public class TopRatedProductDTO
     {
         public int ProductId { get; set; } // Product ID
-      
+        public string ProductName { get; set; } // Product Name
+
     }
 }
