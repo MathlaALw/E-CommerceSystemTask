544c50574f3af72dedd4c5ffeac1284f9a909f0c
Amani Khalid <amanikhalidd@gmail.com>
2025-09-03 09:21:33 +0400

    Modified AddUser

--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -4,7 +4,8 @@ namespace E_CommerceSystem.Services
 {
     public interface IUserService
     {
-        void AddUser(User user);
+
+        void AddUser(UserDTO userDTO); 
         void DeleteUser(int uid);
         IEnumerable<User> GetAllUsers();
         User GetUSer(string email, string password);
