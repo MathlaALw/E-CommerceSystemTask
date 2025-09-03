e6896d1e85346a39490ded3e51c95c53a2dff408
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-09-03 14:02:20 +0400

    Declares a method to generate a new RefreshToken.      It takes the IP address of the client and returns a RefreshToken object

--- Services/ITokenService.cs
+++ Services/ITokenService.cs
@@ -6,7 +6,8 @@ namespace E_CommerceSystem.Services
     {
         string GenerateJwtToken(User user); // Declares a method to create a new JWT (JSON Web Token) for a user.
                                             // It takes a User object and returns the generated JWT as a string
-        RefreshToken GenerateRefreshToken(string ipAddress);
+        RefreshToken GenerateRefreshToken(string ipAddress);  // Declares a method to generate a new RefreshToken.
+                                                              // It takes the IP address of the client and returns a RefreshToken object
         void RemoveTokenCookies(HttpResponse response);
         void SetTokenCookies(HttpResponse response, string jwtToken, RefreshToken refreshToken);
     }
