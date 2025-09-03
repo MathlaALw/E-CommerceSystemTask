d3f81fc877b20338061dce080e81660c0c2d3efd
“ZaharDeveloper” <zalhabsi1994@hotmail.com>
2025-09-03 10:50:57 +0400

    Constructor injecting ApplicationDbContext dependency

--- Repositories/RefreshTokenRepo.cs
+++ Repositories/RefreshTokenRepo.cs
@@ -3,5 +3,11 @@
     public class RefreshTokenRepo
     {
         private readonly ApplicationDbContext _context;  // EF Core DbContext for database access
+
+       // Constructor injecting ApplicationDbContext dependency
+        public RefreshTokenRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
     }
 }
